9543d422c8ff5d577ff151c302ffba9fcb820194
Davvos11 <vosdavid2@gmail.com>
2024-11-02 17:53:49 +0100

    Fix bugs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use rand::Rng;
 use socket2::{Domain, Protocol, SockAddr, Socket, Type};
 
 fn main() {
-    let img = image::open("vck_logo_square_small.png").expect("Failed to open image");
+    let img = image::open("vck_dm.png").expect("Failed to open image");
     let rgba_data = img.to_rgba8().into_raw();
     let (width, height) = img.dimensions();
     let (width, height) = (width as usize, height as usize);
@@ -15,20 +15,21 @@ fn main() {
 
     let addresses: Vec<_> = rgba_data
         .chunks(4)
-        .filter(|rgba| rgba[3] > 0)
         .enumerate()
+        .filter(|(_, rgba)| rgba[3] > 0)
         .map(|(i, rgba)| {
             let x = coords.0 + (i % width);
             let y = coords.1 + (i / width);
             let addr = Ipv6Addr::new(0x2001, 0x610, 0x1908, 0xa000,
-                          x as u16, y as u16,
-                                     0,
-                                     (rgba[0] as u16 * 256) + rgba[3] as u16);
+                                     x as u16, y as u16,
+                                     (rgba[2] as u16) << 8 | rgba[1] as u16,
+                                     (rgba[0] as u16) << 8 | rgba[3] as u16,
+            );
             SocketAddr::new(addr.into(), 0)
         })
         .collect();
 
-    dbg!(&addresses[0..50]);
+    dbg!(addresses.len());
 
     let socket = Socket::new(Domain::IPV6, Type::DGRAM, Some(Protocol::ICMPV6))
         .expect("Could not open socket");
@@ -49,7 +50,6 @@ fn main() {
         let address = addresses[random_index];
 
         socket.send_to(&packet, &SockAddr::from(address)).expect("Failed to send packet");
-        println!("Ping...");
-        sleep(Duration::from_millis(100));
+        sleep(Duration::from_millis(10));
     }
 }
